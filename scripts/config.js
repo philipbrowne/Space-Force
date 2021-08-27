cd532112d12843d3ea0048c25ebcf400d3e5653f
Philip Browne <pbrowne@gmail.com>
2021-08-27 15:27:29 -0400

    reduce game gravity to 700

--- scripts/config.js
+++ scripts/config.js
@@ -14,7 +14,7 @@ const config = {
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 750 },
+      gravity: { y: 700 },
       debug: false,
       debugShowVelocity: true,
       debugShowBody: true,
