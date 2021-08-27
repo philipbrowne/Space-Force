66cfea6343aaf748fd1075fcc08f8b8f37aa471d
Philip Browne <pbrowne@gmail.com>
2021-08-27 08:55:39 -0400

    improve collision box offset for hero

--- scripts/hero.js
+++ scripts/hero.js
@@ -10,9 +10,9 @@ class Hero extends Phaser.GameObjects.Sprite {
     this.body.setCollideWorldBounds(true);
     this.body.setSize(65, 140);
     this.dirKeys = scene.cursorKeys;
-    this.aKey = scene.input.keyboard.addKey('A');
-    this.dKey = scene.input.keyboard.addKey('D');
-    this.wKey = scene.input.keyboard.addKey('W');
+    this.aKey = scene.aKey;
+    this.dKey = scene.dKey;
+    this.wKey = scene.wKey;
   }
   preUpdate(time, delta) {
     super.preUpdate(time, delta);
@@ -20,11 +20,11 @@ class Hero extends Phaser.GameObjects.Sprite {
     if (this.dirKeys.left.isDown || this.aKey.isDown) {
       this.body.setVelocityX(-250);
       this.setFlipX(true);
-      this.body.offset.x = 8;
+      this.body.offset.x = 25;
     } else if (this.dirKeys.right.isDown || this.dKey.isDown) {
       this.body.setVelocityX(250);
       this.setFlipX(false);
-      this.body.offset.x = 12;
+      this.body.offset.x = 20;
     } else {
       this.body.setVelocityX(0);
     }
